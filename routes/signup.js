c82119aa648aafd8da1e2510544c1ef7a977331a
michaelchang106 <71532509+michaelchang106@users.noreply.github.com>
2021-10-31 22:17:24 -0700

    deleted files

--- routes/signup.js
+++ routes/signup.js
@@ -1,38 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+// <<<<<<< HEAD
 const dbManager = require("../database/dbManager.js");
 
 router.get("/", function (req, res) {
-  res.render("signup");
+  res.render("signUp");
 });
 
 //Save user info.
 router.post("/create", async (req, res) => {
-  if (req.body.password != req.body.confirmPassword) {
-    res.send({ passwordMismatch: true });
-  } else {
-    await bcrypt.hash(req.body.password, 10, (err, hash) => {
-      if (err) {
-        throw err;
+  console.log("at create");
+
+  await bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      throw err;
+    }
+    console.log("encrypting password");
+
+    if (req.body.password != req.body.confirmPassword) {
+      res.send({ passwordMismatch: true });
+    }
+    req.body.password = hash;
+    req.body.confirmPassword = hash;
+
+    dbManager.addUser(req.body).then((userExist) => {
+      if (userExist) {
+        res.send({
+          passwordMismatch: false,
+          success: false,
+        });
+      } else {
+        res.send({ passwordMismatch: false, success: true });
       }
-      console.log("encrypting password");
-
-      req.body.password = hash;
-      req.body.confirmPassword = hash;
-
-      dbManager.addUser(req.body).then((userExist) => {
-        if (userExist) {
-          res.send({
-            passwordMismatch: false,
-            success: false,
-          });
-        } else {
-          res.send({ passwordMismatch: false, success: true });
-        }
-      });
     });
-  }
+  });
 });
 
 module.exports = router;
