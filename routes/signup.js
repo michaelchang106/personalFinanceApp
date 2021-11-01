77e7aaf06928208f5192523a6c1bcd6e047b61cb
Daniel Lisko <djlisko01@Daniels-Air.hsd1.ca.comcast.net>
2021-10-31 20:22:38 -0700

    fixed password mismatch

--- routes/signup.js
+++ routes/signup.js
@@ -9,31 +9,30 @@ router.get("/", function (req, res) {
 
 //Save user info.
 router.post("/create", async (req, res) => {
-  console.log("at create");
-
-  await bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      throw err;
-    }
-    console.log("encrypting password");
-
-    if (req.body.password != req.body.confirmPassword) {
-      res.send({ passwordMismatch: true });
-    }
-    req.body.password = hash;
-    req.body.confirmPassword = hash;
-
-    dbManager.addUser(req.body).then((userExist) => {
-      if (userExist) {
-        res.send({
-          passwordMismatch: false,
-          success: false,
-        });
-      } else {
-        res.send({ passwordMismatch: false, success: true });
+  if (req.body.password != req.body.confirmPassword) {
+    res.send({ passwordMismatch: true });
+  } else {
+    await bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
+        throw err;
       }
+      console.log("encrypting password");
+
+      req.body.password = hash;
+      req.body.confirmPassword = hash;
+
+      dbManager.addUser(req.body).then((userExist) => {
+        if (userExist) {
+          res.send({
+            passwordMismatch: false,
+            success: false,
+          });
+        } else {
+          res.send({ passwordMismatch: false, success: true });
+        }
+      });
     });
-  });
+  }
 });
 
 module.exports = router;
