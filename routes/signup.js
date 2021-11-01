542554ac17c5bedfcd612d42b8a0d8c77ca58479
michaelchang106 <71532509+michaelchang106@users.noreply.github.com>
2021-10-31 18:25:25 -0700

    cleaned up uncessary files from merge with firstDraft

--- routes/signup.js
+++ routes/signup.js
@@ -1,30 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
-const UserLogin = require("../database/userSchema.js");
+const dbManager = require("../database/dbManager.js");
 
-mongoose.connect("mongodb://localhost/UserLoginDB");
-
-/* Renders the signup page*/
 router.get("/", function (req, res) {
   res.render("signup");
 });
 
 //Save user info.
 router.post("/create", async (req, res) => {
-  const record = req.body;
-  console.log(record);
-  await UserLogin.create(record).catch((err) => {
-    res.send("ERROR");
+  console.log("at create");
+
+  await bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      throw err;
+    }
+    console.log("encrypting password");
+
+    if (req.body.password != req.body.confirmPassword) {
+      res.send({ passwordMismatch: true });
+    }
+    req.body.password = hash;
+    req.body.confirmPassword = hash;
+
+    dbManager.addUser(req.body).then((userExist) => {
+      if (userExist) {
+        res.send({
+          passwordMismatch: false,
+          success: false,
+        });
+      } else {
+        res.send({ passwordMismatch: false, success: true });
+      }
+    });
   });
-  res.redirect("/signup");
 });
-
-router.get("/getUser", async (req, res) => {
-  const record = await UserLogin.find({ userID: "cats" });
-  res.json(record);
-});
-
 
 module.exports = router;
